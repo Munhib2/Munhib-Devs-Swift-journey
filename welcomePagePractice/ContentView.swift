91ba51b52cc73c77c7a8895feeffeabb69eec1e2
Munhib Devs <munhib219@gmail.com>
2026-07-19 00:59:38 +0500

    Basic Music Player Pagen

--- welcomePagePractice/ContentView.swift
+++ welcomePagePractice/ContentView.swift
@@ -1,24 +1,44 @@
-//
-//  ContentView.swift
-//  welcomePagePractice
-//
-//  Created by Munhib on 18/07/2026.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    var body: some View
+    {
+        ZStack
+        {
+            Color.black
+                .ignoresSafeArea()
+            VStack (spacing: 20)
+            {
+                Spacer()
+                Text ("🎧")
+                    .font(.system(size: 60))
+                    .bold()
+                Spacer()
+                Text ("Unlock Premium Music")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .bold()
+                Text ("Listen to millions of songs offline and without ads")
+                    .font(.body)
+                    .foregroundColor(.white.opacity(0.7))
+                Spacer()
+                Button(action: {})
+                {
+                    Text ("Subscribe now")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .shadow(color: .green.opacity(0.5), radius: 15, x: 0, y: 0)
+                }.padding(.horizontal, 40)
+            }
         }
-        .padding()
     }
 }
-
-#Preview {
+#Preview
+{
     ContentView()
 }
